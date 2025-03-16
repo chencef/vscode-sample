66ca02ac7cec568339b9af73373c0ba1fd0f5b82
chencef <chencef@gmail.com>
2025-03-16 16:11:03 +0800

    first commit

---
+++ Untitled-1.py
@@ -0,0 +1,17 @@
+#產生一個 Streamlit 範例程式
+
+import requests
+import streamlit as st
+
+# 標題
+st.title("Hello, Streamlit!")
+
+# 副標題
+st.subheader("這是一個簡單的範例程式")
+
+# 輸入框
+name = st.text_input("請輸入你的名字：")
+
+# 按鈕
+if st.button("提交"):
+    st.write(f"你好, {name}!")
